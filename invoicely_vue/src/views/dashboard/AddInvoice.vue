809dc3117d8a73a9a1a2e84eed6e728145c54ec2
Muli Job <munyoki912@gmail.com>
2025-02-19 20:50:21 +0300

    CREATING PAGE FOR ADDING iNVOICE

---
+++ invoicely_vue/src/views/dashboard/AddInvoice.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page-add-invoice">
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h1 class="title">Add Invoices</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { toast } from 'bulma-toast';
+
+export default {
+  name: "AddInvoice",
+  data() {
+    return {
+      invoice: {}
+    }
+  }
+}
+</script>
