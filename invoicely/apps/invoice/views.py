961a7a4eb97c3c85ffb934d86dd75143c3538266
Muli Job <munyoki912@gmail.com>
2025-02-28 13:12:15 +0300

    added views for generating pdf

--- invoicely/apps/invoice/views.py
+++ invoicely/apps/invoice/views.py
@@ -1,9 +1,21 @@
-from rest_framework import viewsets
+import pdfkit
+
+from rest_framework import viewsets, status, authentication, permissions
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.response import Response
 
 from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.template.loader import get_template
+
+from apps.team.models import Team
 
 from .serializers import InvoiceSerializer
 from .models import Invoice
+
+
+
 
 
 class InvoiceViewSet(viewsets.ModelViewSet):
@@ -34,3 +46,19 @@ class InvoiceViewSet(viewsets.ModelViewSet):
             raise PermissionDenied("Wrong object owner")
 
         serializer.save()
+
+@api_view(['GET'])
+@authentication_classes([authentication.TokenAuthentication])
+@permission_classes([permissions.IsAuthenticated])
+def generate_pdf(request, invoice_id):
+    """Generating PDF"""
+    invoice = get_object_or_404(Invoice, pk=invoice_id, created_by=request.user)
+    team = Team.objects.filter(created_by=request.user).first()
+    template = get_template('pdf.html')
+    html = template.render({'invoice': invoice, 'team': team})
+    pdf = pdfkit.from_string(html, False, options={})
+
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="invoice.pdf"'
+
+    return response
