0268aa9b8ea8ae015c1b6687c1239c0dbbee6bf6
Muli Job <munyoki912@gmail.com>
2025-03-01 21:08:55 +0300

    showing paid and unpaid invoices on client detail page

--- invoicely/apps/client/serializers.py
+++ invoicely/apps/client/serializers.py
@@ -1,9 +1,29 @@
 from rest_framework import serializers
 
 from .models import Client
+from apps.invoice.models import Invoice
+
+class ClientInvoiceSerializer(serializers.ModelSerializer):
+    """Client Invoice"""
+    class Meta:
+        """class meta"""
+        model = Invoice
+        fields = (
+            "id",
+            "invoice_number",
+            "is_sent",
+            "is_paid",
+            "gross_amount",
+            "vat_amount",
+            "net_amount",
+            "get_due_date_formatted",
+            "invoice_type",
+            "is_credited"
+        )
 
 class ClientSerializer(serializers.ModelSerializer):
     """Client Serializers"""
+    invoices = ClientInvoiceSerializer(many=True)
     class Meta:
         """Class Meta"""
         model = Client
@@ -23,4 +43,5 @@ class ClientSerializer(serializers.ModelSerializer):
             "country",
             "contact_person",
             "contact_reference",
+            "invoices",
         )
